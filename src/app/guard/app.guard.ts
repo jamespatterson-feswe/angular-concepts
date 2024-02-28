ff42079cc5ff0eb06f8c3eabf1a543d5365efe3e
James Patterson <jamespatterson.feswe@yahoo.com>
2024-02-28 01:44:16 -0500

    chore: add app guard, next service

---
+++ src/app/guard/app.guard.ts
@@ -0,0 +1,3 @@
+import { CanActivateFn } from '@angular/router';
+
+export const appGuard: CanActivateFn = (route, state) => true;
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent, SignUpComponent } from './components';
 
 @NgModule({
   declarations: [],
-  exports: [LoginComponent, SignUpComponent],
-  imports: [CommonModule, LoginRoutingModule, LoginComponent, SignUpComponent],
+  exports: [],
+  imports: [CommonModule, LoginRoutingModule],
 })
 export class LoginModule {}
